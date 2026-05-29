d76c4a99d1da6ec26e0e7934b26d6b9d01389fda
SRUJAN CHINTA <76722847+srujan2031@users.noreply.github.com>
2026-05-28 19:19:59 -0500

    Change LinkedIn URL in Contact.tsx
    Updated LinkedIn profile URL in the Contact component.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -10,7 +10,7 @@ const contactLinks = [
     {
         icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" /><circle cx="4" cy="4" r="2" /></svg>,
         text: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/chinta-srujan-882170191/'
+        href: 'https://www.linkedin.com/in/srujanchinta82/'
     },
     {
         icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22" /></svg>,
